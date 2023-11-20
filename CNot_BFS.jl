78288a4eb8d561e04e7088675509ae70fdf28220
peter-janderks <peter-janderks@hotmail.com>
2023-11-20 16:27:49 +0100

    formatting

--- CNot_BFS.jl
+++ CNot_BFS.jl
@@ -8,11 +8,11 @@ module CNot_BFS
 	Two nodes will be connected by an edge iff the stabiliser states
 	can be transformed into one another using a single CNot on a
 	user-provided layout. 
-	
+
 	If no layout is provided, all-to-all connectivity will be assumed.
 =#
 
-import ImplicitGraphs as IG 
+import ImplicitGraphs as IG
 import IterTools as IT
 import QuantumClifford as QC
 
@@ -25,33 +25,33 @@ Also, we can't have our qubits permuted if we're trying to stick to a
 layout.
 """
 function canonical_state(state)
-	QC.canonicalize_rref!(copy(state))[1]
+    QC.canonicalize_rref!(copy(state))[1]
 end
 
-function cnot_network(initial_state, final_state, layout=nothing)
-	gate_path(state_path(initial_state, final_state, layout))
+function cnot_network(initial_state, final_state, layout = nothing)
+    gate_path(state_path(initial_state, final_state, layout))
 end
 
-function possible_cnots(n_qubits, layout=nothing)
-	if isnothing(layout)
-		layout = collect(IT.subsets(1 : n_qubits, 2))
-	end 
+function possible_cnots(n_qubits, layout = nothing)
+    if isnothing(layout)
+        layout = collect(IT.subsets(1:n_qubits, 2))
+    end
 
-	layout = vcat(layout, map(reverse, layout))
-	map(pair -> QC.sCNOT(pair...), layout)
+    layout = vcat(layout, map(reverse, layout))
+    map(pair -> QC.sCNOT(pair...), layout)
 end
 
 function neighbour_state(current_state, gate)
-	canonical_state(gate * current_state)
+    canonical_state(gate * current_state)
 end
 
 function possible_neighbours(layout)
-	neighbs(current_state) = begin
-		cnots = possible_cnots(QC.nqubits(current_state), layout)
-		map(cnot -> neighbour_state(current_state, cnot), cnots)
-	end
+    neighbs(current_state) = begin
+        cnots = possible_cnots(QC.nqubits(current_state), layout)
+        map(cnot -> neighbour_state(current_state, cnot), cnots)
+    end
 
-	neighbs
+    neighbs
 end
 
 @inline weight(p::QC.PauliOperator) = count_ones(p.xz[1] | p.xz[2])
@@ -64,23 +64,23 @@ checking whether every stabilizer is weight-one after the state has
 been canonicalized.  
 """
 function is_separable(state)
-	all(map(p -> weight(p) == 1, canonical_state(state)))
+    all(map(p -> weight(p) == 1, canonical_state(state)))
 end
 
-function state_path(start, target::QC.Stabilizer, layout=nothing, max_states=0)
-	#=
-		For now, I'm going to try assuming that only edge search generates
-		new vertices
-	=#
-	graph = IG.ImplicitGraph{QC.Stabilizer}(anything -> true, possible_neighbours(layout))
+function state_path(start, target::QC.Stabilizer, layout = nothing, max_states = 0)
+    #=
+    		For now, I'm going to try assuming that only edge search generates
+    		new vertices
+    	=#
+    graph = IG.ImplicitGraph{QC.Stabilizer}(anything -> true, possible_neighbours(layout))
 
-	IG.find_path(graph, canonical_state(start), canonical_state(target), max_states)
+    IG.find_path(graph, canonical_state(start), canonical_state(target), max_states)
 end
 
-function state_path(start, target::Function, layout=nothing, max_states=0)
-	graph = IG.ImplicitGraph{QC.Stabilizer}(anything -> true, possible_neighbours(layout))
-	
-	IG.find_path(graph, canonical_state(start), target, max_states)
+function state_path(start, target::Function, layout = nothing, max_states = 0)
+    graph = IG.ImplicitGraph{QC.Stabilizer}(anything -> true, possible_neighbours(layout))
+
+    IG.find_path(graph, canonical_state(start), target, max_states)
 end
 
 """
@@ -91,21 +91,21 @@ final state, but it doesn't tell us which CNot we used to get from each
 state to the next. This function brute-force iterates over the possible
 CNots to determine the one that takes you from each vertex to the next. 
 """
-function gate_path(state_path, layout=nothing)
-	cnots = possible_cnots(QC.nqubits(state_path[1]), layout)
-	
-	edge_cnot(dx) = begin
-		in_state = state_path[dx]
-		out_state = state_path[dx + 1]
-		for cnot in cnots
-			if neighbour_state(in_state, cnot) == out_state
-				return cnot
-			end
-		end
-		error("no CNot found")
-	end
+function gate_path(state_path, layout = nothing)
+    cnots = possible_cnots(QC.nqubits(state_path[1]), layout)
 
-	map(edge_cnot, 1 : length(state_path) - 1)
+    edge_cnot(dx) = begin
+        in_state = state_path[dx]
+        out_state = state_path[dx+1]
+        for cnot in cnots
+            if neighbour_state(in_state, cnot) == out_state
+                return cnot
+            end
+        end
+        error("no CNot found")
+    end
+
+    map(edge_cnot, 1:length(state_path)-1)
 end
 
 end # module CNot_BFS
--- CNot_BFS_Sandbox.jl
+++ CNot_BFS_Sandbox.jl
@@ -9,80 +9,138 @@ plus_zero_3 = QC.Stabilizer([QC.P"XIII", QC.P"IZII", QC.P"IIZI", QC.P"IIIZ"])
 cat_state = QC.Stabilizer([QC.P"XXXX", QC.P"ZZII", QC.P"IZZI", QC.P"IIZZ"])
 
 sc3_initial_state = QC.Stabilizer([
-									QC.P"ZIIIIIIII", QC.P"IXIIIIIII", QC.P"IIZIIIIII",
-									QC.P"IIIXIIIII", QC.P"IIIIZIIII", QC.P"IIIIIXIII",
-									QC.P"IIIIIIZII", QC.P"IIIIIIIXI", QC.P"IIIIIIIIZ"
-								])
+    QC.P"ZIIIIIIII",
+    QC.P"IXIIIIIII",
+    QC.P"IIZIIIIII",
+    QC.P"IIIXIIIII",
+    QC.P"IIIIZIIII",
+    QC.P"IIIIIXIII",
+    QC.P"IIIIIIZII",
+    QC.P"IIIIIIIXI",
+    QC.P"IIIIIIIIZ",
+])
 sc3_final_state = QC.Stabilizer([
-									QC.P"ZZZIIIIII", QC.P"XXIIIIIII", QC.P"IIZZIIIII",
-									QC.P"IXXXXIIII", QC.P"IIIZZZIII", QC.P"IIIIXXXXI",
-									QC.P"IIIIIZZII", QC.P"IIIIIIIXX", QC.P"IIIIIIZZZ"
-								])
+    QC.P"ZZZIIIIII",
+    QC.P"XXIIIIIII",
+    QC.P"IIZZIIIII",
+    QC.P"IXXXXIIII",
+    QC.P"IIIZZZIII",
+    QC.P"IIIIXXXXI",
+    QC.P"IIIIIZZII",
+    QC.P"IIIIIIIXX",
+    QC.P"IIIIIIZZZ",
+])
 
 cube_initial_state = QC.Stabilizer([
-									QC.P"ZIIIIIII", QC.P"IXIIIIII", QC.P"IIZIIIII",
-									QC.P"IIIXIIII", QC.P"IIIIZIII", QC.P"IIIIIXII",
-									QC.P"IIIIIIZI", QC.P"IIIIIIIX"
-								])
+    QC.P"ZIIIIIII",
+    QC.P"IXIIIIII",
+    QC.P"IIZIIIII",
+    QC.P"IIIXIIII",
+    QC.P"IIIIZIII",
+    QC.P"IIIIIXII",
+    QC.P"IIIIIIZI",
+    QC.P"IIIIIIIX",
+])
 cube_final_state = QC.Stabilizer([
-									QC.P"ZZZZIIII", QC.P"XXXXIIII", QC.P"ZZIIZZII",
-									QC.P"XXIIXXII", QC.P"ZIZIZIZI", QC.P"XIXIXIXI",
-									QC.P"IIIIZZZZ", QC.P"IIIIXXXX"
-								])
+    QC.P"ZZZZIIII",
+    QC.P"XXXXIIII",
+    QC.P"ZZIIZZII",
+    QC.P"XXIIXXII",
+    QC.P"ZIZIZIZI",
+    QC.P"XIXIXIXI",
+    QC.P"IIIIZZZZ",
+    QC.P"IIIIXXXX",
+])
 
 cube_initial_state_2 = QC.Stabilizer([
-									QC.P"IIZIIIII", QC.P"IIIXIIII", QC.P"IZIIIIII",
-									QC.P"IIIIIXII", QC.P"ZIIIIIII", QC.P"IIIIXIII",
-									QC.P"IIIIIIZI", QC.P"IIIIIIIX"
-								])
+    QC.P"IIZIIIII",
+    QC.P"IIIXIIII",
+    QC.P"IZIIIIII",
+    QC.P"IIIIIXII",
+    QC.P"ZIIIIIII",
+    QC.P"IIIIXIII",
+    QC.P"IIIIIIZI",
+    QC.P"IIIIIIIX",
+])
 cube_final_state_2 = QC.Stabilizer([
-									QC.P"ZZZZIIII", QC.P"XXXXIIII", QC.P"ZZIIZZII",
-									QC.P"XXIIXXII", QC.P"ZIZIZIZI", QC.P"XIXIXIXI",
-									QC.P"IIIIZZZZ", QC.P"IIIIXXXX"
-								])
+    QC.P"ZZZZIIII",
+    QC.P"XXXXIIII",
+    QC.P"ZZIIZZII",
+    QC.P"XXIIXXII",
+    QC.P"ZIZIZIZI",
+    QC.P"XIXIXIXI",
+    QC.P"IIIIZZZZ",
+    QC.P"IIIIXXXX",
+])
 cube_bell_states = QC.Stabilizer([
-									QC.P"ZIIIZIII", QC.P"XIIIXIII",
-									QC.P"IZIIIZII", QC.P"IXIIIXII", 
-									QC.P"IIZZIIII", QC.P"IIXXIIII", 
-									QC.P"IIIIIIZZ", QC.P"IIIIIIXX"
-								])
+    QC.P"ZIIIZIII",
+    QC.P"XIIIXIII",
+    QC.P"IZIIIZII",
+    QC.P"IXIIIXII",
+    QC.P"IIZZIIII",
+    QC.P"IIXXIIII",
+    QC.P"IIIIIIZZ",
+    QC.P"IIIIIIXX",
+])
 
 cube_bell_states_2 = QC.Stabilizer([
-									QC.P"ZZIIIIII", QC.P"XXIIIIII",
-									QC.P"IIZZIIII", QC.P"IIXXIIII", 
-									QC.P"IIIIZZII", QC.P"IIIIXXII", 
-									QC.P"IIIIIIZZ", QC.P"IIIIIIXX"
-								])
+    QC.P"ZZIIIIII",
+    QC.P"XXIIIIII",
+    QC.P"IIZZIIII",
+    QC.P"IIXXIIII",
+    QC.P"IIIIZZII",
+    QC.P"IIIIXXII",
+    QC.P"IIIIIIZZ",
+    QC.P"IIIIIIXX",
+])
 
 cube_GHZ_states = QC.Stabilizer([
-									QC.P"XXIIIIII", QC.P"XIXIIIII",
-									QC.P"ZZZIIIII", QC.P"IIIIZZII", 
-									QC.P"IIIIZIZI", QC.P"IIIIXXXI", 
-									QC.P"IIIZIIIZ", QC.P"IIIXIIIX"
-								])
+    QC.P"XXIIIIII",
+    QC.P"XIXIIIII",
+    QC.P"ZZZIIIII",
+    QC.P"IIIIZZII",
+    QC.P"IIIIZIZI",
+    QC.P"IIIIXXXI",
+    QC.P"IIIZIIIZ",
+    QC.P"IIIXIIIX",
+])
 
 cube_GHZ_states_2 = QC.Stabilizer([
-									QC.P"ZZIIIIII", QC.P"ZIZIIIII",
-									QC.P"XXXIIIII", QC.P"IIIIZZII", 
-									QC.P"IIIIZIZI", QC.P"IIIIXXXI", 
-									QC.P"IIIXIIII", QC.P"IIIIIIIX"
-								])
+    QC.P"ZZIIIIII",
+    QC.P"ZIZIIIII",
+    QC.P"XXXIIIII",
+    QC.P"IIIIZZII",
+    QC.P"IIIIZIZI",
+    QC.P"IIIIXXXI",
+    QC.P"IIIXIIII",
+    QC.P"IIIIIIIX",
+])
 
 cube_whole_space = QC.Stabilizer([
-									QC.P"ZZIIIIII", QC.P"ZIZIIIII",
-									QC.P"XXXIIIII", QC.P"IIIIZZII", 
-									QC.P"IIIIZIZI", QC.P"IIIIXXXI", 
-									QC.P"IIIXIIII", QC.P"IIIIIIIX"
-								])
+    QC.P"ZZIIIIII",
+    QC.P"ZIZIIIII",
+    QC.P"XXXIIIII",
+    QC.P"IIIIZZII",
+    QC.P"IIIIZIZI",
+    QC.P"IIIIXXXI",
+    QC.P"IIIXIIII",
+    QC.P"IIIIIIIX",
+])
 
 planar_832_plus_state = QC.Stabilizer([
-									QC.P"ZZZZIIIIIIII", QC.P"XXXXIIIIIIII",
-									QC.P"ZZIIZZIIIIII", QC.P"XXIIXXIIIIII",
-									QC.P"ZIZIZIZIIIII", QC.P"XIXIXIXIIIII",
-									QC.P"IIIIZZZZIIII", QC.P"IIIIXXXXIIII",
-									QC.P"IIIIIIIIZIII", QC.P"IIIIIIIIIZII", 
-									QC.P"IIIIIIIIIIZI", QC.P"IIIIIIIIIIIZ"
-								])
+    QC.P"ZZZZIIIIIIII",
+    QC.P"XXXXIIIIIIII",
+    QC.P"ZZIIZZIIIIII",
+    QC.P"XXIIXXIIIIII",
+    QC.P"ZIZIZIZIIIII",
+    QC.P"XIXIXIXIIIII",
+    QC.P"IIIIZZZZIIII",
+    QC.P"IIIIXXXXIIII",
+    QC.P"IIIIIIIIZIII",
+    QC.P"IIIIIIIIIZII",
+    QC.P"IIIIIIIIIIZI",
+    QC.P"IIIIIIIIIIIZ",
+])
 
 #=
 	Here are some planar layouts that we look at for comparison.
@@ -100,18 +158,24 @@ planar_832_plus_state = QC.Stabilizer([
 =#
 
 sandwich_layout = [
-					[1, 2], [1, 9],
-					[2, 3], [2, 10],
-					[3, 4], [3, 11],
-					[4, 12],
-					[5, 6], [5, 9],
-					[6, 7], [6, 10],
-					[7, 8], [7, 11],
-					[8, 12],
-					[9, 10],
-					[10, 11],
-					[11, 12]
-				]
+    [1, 2],
+    [1, 9],
+    [2, 3],
+    [2, 10],
+    [3, 4],
+    [3, 11],
+    [4, 12],
+    [5, 6],
+    [5, 9],
+    [6, 7],
+    [6, 10],
+    [7, 8],
+    [7, 11],
+    [8, 12],
+    [9, 10],
+    [10, 11],
+    [11, 12],
+]
 
 #=
 	4--6--12
@@ -124,16 +188,24 @@ sandwich_layout = [
 =#
 
 buckle_layout = [
-					[1, 2], [1, 5],
-					[2, 3], [2, 9],
-					[3, 4], [3, 10],
-					[4, 6],
-					[5, 9], [5, 11],
-					[6, 10], [6, 12],
-					[7, 8], [7, 9], [7, 11],
-					[8, 10], [8, 12],
-					[9, 10]
-				]
+    [1, 2],
+    [1, 5],
+    [2, 3],
+    [2, 9],
+    [3, 4],
+    [3, 10],
+    [4, 6],
+    [5, 9],
+    [5, 11],
+    [6, 10],
+    [6, 12],
+    [7, 8],
+    [7, 9],
+    [7, 11],
+    [8, 10],
+    [8, 12],
+    [9, 10],
+]
 
 #=
 	3--5---12
@@ -146,15 +218,23 @@ buckle_layout = [
 =#
 
 s_layout = [
-			[1, 2], [1, 9], [1, 10],
-			[2, 3], [2, 11],
-			[3, 5],
-			[4, 6], [4, 9], [4, 10],
-			[5, 11], [5, 12],
-			[6, 7],
-			[7, 8], [7, 10],
-			[8, 11], [8, 12],
-			[10, 11]
+    [1, 2],
+    [1, 9],
+    [1, 10],
+    [2, 3],
+    [2, 11],
+    [3, 5],
+    [4, 6],
+    [4, 9],
+    [4, 10],
+    [5, 11],
+    [5, 12],
+    [6, 7],
+    [7, 8],
+    [7, 10],
+    [8, 11],
+    [8, 12],
+    [10, 11],
 ]
 
 #=
@@ -167,42 +247,39 @@ s_layout = [
 	1--3
 =#
 
-tiny_layout = [
-			[1, 2], [1, 3],
-			[2, 4], [2, 5],
-			[3, 5],
-			[4, 6], [4, 7],
-			[5, 6],
-			[6, 8], 
-			[7, 8]
-]
+tiny_layout =
+    [[1, 2], [1, 3], [2, 4], [2, 5], [3, 5], [4, 6], [4, 7], [5, 6], [6, 8], [7, 8]]
 
 function all_partitions_cube_prep()
-	qubits = collect(1:8)
-	x_partitions = collect(IT.subsets(qubits, 4))
-	z_partitions = map(x_p -> setdiff(qubits, x_p), x_partitions)
-	
-	path_and_length(state) = begin
-		path = CNot_BFS.state_path(state, cube_final_state_2)
-		path, length(path)	
-	end
+    qubits = collect(1:8)
+    x_partitions = collect(IT.subsets(qubits, 4))
+    z_partitions = map(x_p -> setdiff(qubits, x_p), x_partitions)
 
-	init_state = cube_separable_state(x_partitions[1], z_partitions[1])
-	shortest_path, min_length = path_and_length(init_state)
-	
-	for (x_p, z_p) in zip(x_partitions[2:end], z_partitions[2:end])
-		path, length = path_and_length(cube_separable_state(x_p, z_p))
-		if length < min_length
-			shortest_path, min_length = path, length
-		end
-	end 
+    path_and_length(state) = begin
+        path = CNot_BFS.state_path(state, cube_final_state_2)
+        path, length(path)
+    end
 
-	CNot_BFS.gate_path(shortest_path)
+    init_state = cube_separable_state(x_partitions[1], z_partitions[1])
+    shortest_path, min_length = path_and_length(init_state)
+
+    for (x_p, z_p) in zip(x_partitions[2:end], z_partitions[2:end])
+        path, length = path_and_length(cube_separable_state(x_p, z_p))
+        if length < min_length
+            shortest_path, min_length = path, length
+        end
+    end
+
+    CNot_BFS.gate_path(shortest_path)
 end
 
 function cube_separable_state(x_partition, z_partition)
-	QC.Stabilizer(vcat(map(bit -> QC.single_x(8, bit), x_partition),
-		map(bit -> QC.single_z(8, bit), z_partition)))
+    QC.Stabilizer(
+        vcat(
+            map(bit -> QC.single_x(8, bit), x_partition),
+            map(bit -> QC.single_z(8, bit), z_partition),
+        ),
+    )
 end
 
 """
@@ -218,38 +295,44 @@ We search over layouts where the six qubits (1, 2, 4, 5, 6, 7) will be
 adjacent to the two qubits in the middle after a pair of swaps
 """
 function planar_cube_prep()
-	desired_state = cube_final_state
+    desired_state = cube_final_state
 
-	close_qubits = [1, 2, 4, 5, 6, 7]
-	perms_6 = map(perm -> close_qubits[perm], collect(CO.permutations(1:6)))
-	perms_2 = [[3, 8], [8, 3]]
-	ctr = 1
-	solutions = []
-	for perm_6 in perms_6
-		@show ctr
-		for perm_2 in perms_2
-			qs = vcat(perm_6, perm_2)
-			layout = [[qs[1], qs[2]], [qs[1], qs[7]],
-						[qs[2], qs[3]], [qs[2], qs[5]],
-						[qs[3], qs[4]], [qs[3], qs[6]],
-						[qs[4], qs[8]],
-						[qs[5], qs[6]], [qs[5], qs[7]],
-						[qs[6], qs[8]]]
-			
-			path = CNot_BFS.state_path(desired_state, CNot_BFS.is_separable, layout, 12)
-			
-			if length(path) < 1 
-				continue
-			end
-			
-			gates = CNot_BFS.gate_path(path, layout)
-			
-			if length(gates) == 10
-				push!(solutions, (layout, gates)) 
-			end
-		end
-		ctr += 1
-	end
-	
-	solutions
+    close_qubits = [1, 2, 4, 5, 6, 7]
+    perms_6 = map(perm -> close_qubits[perm], collect(CO.permutations(1:6)))
+    perms_2 = [[3, 8], [8, 3]]
+    ctr = 1
+    solutions = []
+    for perm_6 in perms_6
+        @show ctr
+        for perm_2 in perms_2
+            qs = vcat(perm_6, perm_2)
+            layout = [
+                [qs[1], qs[2]],
+                [qs[1], qs[7]],
+                [qs[2], qs[3]],
+                [qs[2], qs[5]],
+                [qs[3], qs[4]],
+                [qs[3], qs[6]],
+                [qs[4], qs[8]],
+                [qs[5], qs[6]],
+                [qs[5], qs[7]],
+                [qs[6], qs[8]],
+            ]
+
+            path = CNot_BFS.state_path(desired_state, CNot_BFS.is_separable, layout, 12)
+
+            if length(path) < 1
+                continue
+            end
+
+            gates = CNot_BFS.gate_path(path, layout)
+
+            if length(gates) == 10
+                push!(solutions, (layout, gates))
+            end
+        end
+        ctr += 1
+    end
+
+    solutions
 end
