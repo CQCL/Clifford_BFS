139b3aed2b82e3d1be2dddd9647e41e0d614722a
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2023-11-24 14:07:49 +0000

    test working

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,8 @@
 using CliffordBFS
 using Test
 
-import CliffordBFS.CNot_BFS as CNot_BFS
 
 @testset "Example tests" begin
-	include("../CNot_BFS_Sandbox.jl")
-	@test path = CNot_BFS.gate_path(CNot_BFS.state_path(plus_zero_3, cat_state))
+    include("../CNot_BFS_Sandbox.jl")
+    @test length(CNot_BFS.gate_path(CNot_BFS.state_path(plus_zero_3, cat_state))) == 3
 end
