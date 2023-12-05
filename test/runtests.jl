dc68a2fad5cd3a1adb93e6c142c1191790ab425b
Ben Criger <bcriger@gmail.com>
2023-12-05 14:18:25 +0000

    fixing up tests so I can test on main in the REPL

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
-using CliffordBFS
+import CliffordBFS as CB
 using Test
 
 
 @testset "Cat State Preparation" begin
-    include("../CNot_BFS_Sandbox.jl")
-    @test length(CNot_BFS.gate_path(CNot_BFS.state_path(plus_zero_3, cat_state))) == 3
+    @test length(CB.gate_path(CB.state_path(plus_zero_3, cat_state))) == 3
 end
