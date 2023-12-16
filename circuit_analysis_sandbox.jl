d724b97791528d3b6105fbe9a31164fada1deb5b
Ben Criger <bcriger@gmail.com>
2023-12-16 16:26:30 +0000

    just going to run the tests one more time before I submit the paper . . .

--- circuit_analysis_sandbox.jl
+++ circuit_analysis_sandbox.jl
@@ -1,35 +1,56 @@
 import CliffordBFS as CB
 import QuantumClifford as QC
 
-"""
-Generates the high-weight errors output from the circuit, and a pair of
-stabilizers we need to measure in order to detect those errors, if such
-a pair exists.
-"""
-function measurements_after_gatelist(gatelist; n_stabs=2)
-	circ = CB.Circuit(gatelist, 8)
+#=
+	In this note, we're going to generate the sets of measurements we
+	need in order to detect high-weight errors, then briefly discuss
+	why we don't actually need to do that.
+=#
 
-	stab_gens = [QC.P"XXXXIIII", QC.P"XXIIXXII",
-					QC.P"XIXIXIXI", QC.P"IIIIXXXX",
-					QC.P"ZZZZIIII", QC.P"ZZIIZZII",
-					QC.P"ZIZIZIZI", QC.P"IIIIZZZZ"]
-
-	stabs_832 = [QC.P"XXXXXXXX", QC.P"ZZZZIIII", QC.P"ZZIIZZII",
-					QC.P"ZIZIZIZI", QC.P"IIIIZZZZ"]
-
-	stab_group = CB.generated_group(stab_gens)
-
-	errs = CB.brute_force_minimize(CB.output_errors(circ), stab_group)
-	
-	#=
-		we need to measure logical operators to detect errors that
-		stabilizer measurement wouldn't detect on its own
-	=#
-	dangerous_errs = CB.undetected_errors(errs, stabs_832)
-	
-	stab_subgroups = [stab_group[dxs] for dxs in
-		CB.postmeasurements(dangerous_errs, stab_group, n_stabs)]
-end
-
+#=
+	As discussed in the paper, the last six gates are the only ones
+	that can create or propagate dangerous errors. 
+=#
 gatelist = map(pr -> QC.sCNOT(pr...),
 				[(1, 2), (2, 3), (2, 7), (3, 1), (7, 1), (1, 2)]);
+circ = CB.Circuit(gatelist, 8)
+all_errs = CB.output_errors(circ)
+
+#=
+	We only need to worry about the weights of errors after they've
+	been multiplied by some appropriate stabilizer. 
+
+	Here, we're going to do that using an exponential-time brute-force
+	algorithm.
+
+	Note that logical Xs are also stabilizers for this purpose. 
+=#
+stab_gens = [QC.P"XXXXIIII", QC.P"XXIIXXII",
+				QC.P"XIXIXIXI", QC.P"IIIIXXXX",
+				QC.P"ZZZZIIII", QC.P"ZZIIZZII",
+				QC.P"ZIZIZIZI", QC.P"IIIIZZZZ"]
+stab_group = CB.generated_group(stab_gens)
+errs = CB.brute_force_minimize(all_errs, stab_group)
+
+#=
+	Let's see which of these errors can't be detected:
+=#
+
+stabs_832 = [QC.P"XXXXXXXX", QC.P"ZZZZIIII", QC.P"ZZIIZZII",
+				QC.P"ZIZIZIZI", QC.P"IIIIZZZZ"]
+dangerous_errs = CB.undetected_errors(errs, stabs_832)
+
+#=
+	We can see that the set is quite small, containing only two logical
+	Z operators. 
+	It's easy to see that an appropriate logical X operator can
+	anticommute with both of them. 
+
+	In an earlier version of this code, the set `dangerous_errs` above
+	was being calculated incorrectly (only weight-1 errors were being
+	produced after a CNot).
+	However, we weren't checking explicitly for undetected errors, but
+	rather errors with weight > 1, resulting in the circuit seen in the
+	paper.
+=#
+
