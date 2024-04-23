c31a6116ac19b57f34f7dc828c9f35e4033537e1
Cobord <ammar.s.husain@gmail.com>
2024-04-23 16:09:12 -0400

    python binding, dependency errors?

--- test_python.py
+++ test_python.py
@@ -1,6 +1,10 @@
+"""
+python binding and a simple test
+"""
+
+# pylint:disable=no-name-in-module, wrong-import-position
 from julia import Pkg
 Pkg.activate(".")
-
 from julia import CliffordBFS
 
 assert CliffordBFS.add1(1) == 2
